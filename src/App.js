e79a78c86c9cb311dd9d22b2f4a5c7e49578725a
Vitaly Tochenyy <Antisvinn@yadnex.by>
2021-11-16 20:43:53 +0300

    task#1 implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
+import Main from './pages/main/Main';
 
-function App() {
-	return <div className='App'>hello</div>;
-}
+const App = () => {
+	return <Main />;
+};
 
 export default App;
---
+++ src/pages/main/Main.js
@@ -0,0 +1,12 @@
+import Header from '../../components/header/Header';
+import './style.css';
+
+const Main = () => {
+	return (
+		<div className='container'>
+			<Header />
+		</div>
+	);
+};
+
+export default Main;
