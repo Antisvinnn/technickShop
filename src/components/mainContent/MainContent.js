b35b659feea30ad8598a2cd8e90a4a6620f6b40c
Vitaly Tochenyy <Antisvinn@yadnex.by>
2021-11-17 17:08:23 +0300

    test protect

--- src/components/mainContent/MainContent.js
+++ src/components/mainContent/MainContent.js
@@ -1,46 +1,46 @@
-import "./style.css";
+import './style.css';
 
 const MainContent = () => {
-  return (
-    <div className="mainBlock">
-      <div>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/ru/3/39/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D1%80%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0.JPG"
-          alt="not found"
-        />
-      </div>
-      <div>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/ru/3/39/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D1%80%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0.JPG"
-          alt="not found"
-        />
-      </div>
-      <div>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/ru/3/39/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D1%80%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0.JPG"
-          alt="not found"
-        />
-      </div>
+	return (
+		<div className='mainBlock'>
+			<div>
+				<img
+					src='https://upload.wikimedia.org/wikipedia/ru/3/39/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D1%80%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0.JPG'
+					alt='not found'
+				/>
+			</div>
+			<div>
+				<img
+					src='https://upload.wikimedia.org/wikipedia/ru/3/39/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D1%80%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0.JPG'
+					alt='not found'
+				/>
+			</div>
+			<div>
+				<img
+					src='https://upload.wikimedia.org/wikipedia/ru/3/39/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D1%80%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0.JPG'
+					alt='not found'
+				/>
+			</div>
 
-      <div>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/ru/3/39/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D1%80%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0.JPG"
-          alt="not found"
-        />
-      </div>
-      <div>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/ru/3/39/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D1%80%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0.JPG"
-          alt="not found"
-        />
-      </div>
-      <div>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/ru/3/39/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D1%80%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0.JPG"
-          alt="not found"
-        />
-      </div>
-    </div>
-  );
+			<div>
+				<img
+					src='https://upload.wikimedia.org/wikipedia/ru/3/39/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D1%80%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0.JPG'
+					alt='not found'
+				/>
+			</div>
+			<div>
+				<img
+					src='https://upload.wikimedia.org/wikipedia/ru/3/39/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D1%80%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0.JPG'
+					alt='not found'
+				/>
+			</div>
+			<div>
+				<img
+					src='https://upload.wikimedia.org/wikipedia/ru/3/39/%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%B5%D1%80%D0%B1%D0%BB%D1%8E%D0%B4%D0%B0.JPG'
+					alt='not found'
+				/>
+			</div>
+		</div>
+	);
 };
 export default MainContent;
