51ee5271ce131199a106f7799e5ad057089110e7
Vlad Ostapuk <Sp0ngbik@gmail.com>
2021-11-19 19:01:19 +0300

    style for storeInfo

--- src/components/IStoreBlockInfo/IStoreInfo.js
+++ src/components/IStoreBlockInfo/IStoreInfo.js
@@ -1,6 +1,7 @@
 import NavBlock from "../navBlcoks/NavBlock";
 import NavLinks from "../navLinks/NavLinks";
 import IStoreParthner from "../storeParthner/IStoreParthner";
+import "./style.css";
 
 const IStoreInfo = () => {
   return (
