6f130e9a3fbc6fd7593a45b528745f827fe15f95
Grigory Rybachuk <grigory-r2008@yandex.ru>
2021-11-21 10:33:59 +0300

    change firstline

--- src/components/devices/Devices.js
+++ src/components/devices/Devices.js
@@ -7,8 +7,10 @@ const Devices = () => {
         <div className={style.wrapperForDevices}>
           <img src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:210:210:0/bg:f6f6f6/plain/s3://files/7/i-store-block-8-by-image-1.jpg@jpeg" />
           <div className={style.deviceContent}>
-            <p>AirPods.</p>
-            <p>Никаких проводов.Никаких сложностей</p>
+            <p className={style.mainContent}>AirPods.</p>
+            <p className={style.descriptionContent}>
+              Никаких проводов.Никаких сложностей
+            </p>
             <a href="$">Купить</a>
           </div>
         </div>
@@ -17,8 +19,10 @@ const Devices = () => {
           <img src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:210:210:0/bg:f6f6f6/plain/s3://files/7/main-catalog-banner-iphone-case.png@jpeg" />
 
           <div className={style.deviceContent}>
-            <p>Чехлы на IPhone 13 и Iphone 13 Pro</p>
-            <p>Только идеальные пары</p>
+            <p className={style.mainContent}>
+              Чехлы на IPhone 13 и Iphone 13 Pro
+            </p>
+            <p className={style.descriptionContent}>Только идеальные пары</p>
             <a href="$">Заказать</a>
           </div>
         </div>
@@ -26,8 +30,10 @@ const Devices = () => {
           <img src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:210:210:0/bg:f6f6f6/plain/s3://files/7/i-store-block-8-by-image-2.png@jpeg" />
 
           <div className={style.deviceContent}>
-            <p>Карта клиента.</p>
-            <p>Совершайте покупки и получайте бонусы на счет</p>
+            <p className={style.mainContent}>Карта клиента.</p>
+            <p className={style.descriptionContent}>
+              Совершайте покупки и получайте бонусы на счет
+            </p>
             <a href="$">Подробнее</a>
           </div>
         </div>
