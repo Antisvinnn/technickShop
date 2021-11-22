e16c572f6cceb340c9dd7734c81a106bb1858bfd
Vlad Ostapuk <Sp0ngbik@gmail.com>
2021-11-22 17:33:37 +0300

    task#1Try

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -3,10 +3,12 @@ import style from "./style.module.css";
 import IStoreInfo from "../../components/IStoreBlockInfo/IStoreInfo";
 import Devices from "../../components/devices/Devices";
 import DeliveryWays from "../../components/iStoreWaysDelivery/DeliveryWays";
+import Header from "../../components/header/Header";
 
 const Main = () => {
   return (
     <div className={style.container}>
+      <Header />
       <Devices />
       <IStoreInfo />
       <DeliveryWays />
