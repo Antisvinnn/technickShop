4c328d5e545607a53a64b38211346975f1a3ae3b
Grigory Rybachuk <grigory-r2008@yandex.ru>
2021-11-27 15:31:29 +0300

    task#9

--- src/pages/infoRoadMap/InfoRoadMap.js
+++ src/pages/infoRoadMap/InfoRoadMap.js
@@ -1,7 +1,5 @@
 import { Route, Switch } from "react-router";
-
 import Credits from "../credits/Credits";
-
 import Information from "../information/Information";
 import About from "../about/About";
 
@@ -9,14 +7,9 @@ const InfoRoadMap = () => {
   return (
     <>
       <Switch>
-
         <Route path="/information/about" component={About} />
-     
-
-     
-        <Route path="/information/installment-and-credit" component={Credits} />  
-          <Route path="/information" component={Information} />
-
+        <Route path="/information/installment-and-credit" component={Credits} />
+        <Route path="/information" component={Information} />
       </Switch>
     </>
   );
