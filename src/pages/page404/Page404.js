e8b5f6dcc4728933db5c95792587a92155101c33
Grigory Rybachuk <grigory-r2008@yandex.ru>
2021-11-29 21:37:16 +0300

    added page 404

--- src/pages/page404/Page404.js
+++ src/pages/page404/Page404.js
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom";
+import style from "./style.module.css";
+
 const Page404 = () => {
-	return (
-		<div>
-			<div>404 not found</div>
-		</div>
-	);
+  return (
+    <>
+      <div className={style.page404}>
+        <h1>Ошибка 404</h1>
+        <p>
+          Кажется что-то пошло не так! Страница, которую вы запрашиваете, не
+          существует! Возможно, она устарела, была удалена, или был введен
+          неверный адрес в адресной строке.
+        </p>
+        <Link to="/">Перейти на главную</Link>
+      </div>
+    </>
+  );
 };
 
 export default Page404;
