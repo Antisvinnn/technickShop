3a0c082c1ae9c161e790cf62ec5713b4f67b10f1
Grigory Rybachuk <grigory-r2008@yandex.ru>
2021-12-06 22:58:00 +0300

    was added method of array - map

--- src/pages/iphone/Iphone.js
+++ src/pages/iphone/Iphone.js
@@ -1,7 +1,21 @@
 import style from "./style.module.scss";
 import house from "../../assets/images/credits/pngegg.png";
+import IphoneConfig from "./iphone.config";
 
 const Iphone = () => {
+  const ourIphone = (arr) => {
+    return arr.map((el, index) => {
+      return (
+        <div key={index} className={el.classNameContainer}>
+          <p className={el.classNameForState}>{el.state}</p>
+          <div className={el.classNameForImageWithText}>
+            <img src={el.image} alt="not found" />
+            <p>{el.title}</p>
+          </div>
+        </div>
+      );
+    });
+  };
   return (
     <>
       <div class={style.container}>
@@ -21,126 +35,7 @@ const Iphone = () => {
           </div>
         </div>
       </div>
-      <div className={style.listOfIphone}>
-        <div className={style.containerForIphone}>
-          <p className={style.new}>NEW</p>
-          <div className={style.imageWithText}>
-            <img
-              src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/13859/ip13_pro.png@jpeg"
-              alt="not found"
-            />
-            <p>iPhone 13 Pro</p>
-          </div>
-        </div>
-        <div className={style.containerForIphone}>
-          <p className={style.new}>NEW</p>
-          <div className={style.imageWithText}>
-            <img
-              src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/13860/ip13_promax.png@webp"
-              alt="not found"
-            />
-            <p>iPhone 13 Pro Max</p>
-          </div>
-        </div>
-        <div className={style.containerForIphone}>
-          <p className={style.new}>NEW</p>
-          <div className={style.imageWithText}>
-            <img
-              src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/13857/ip13.png@webp"
-              alt="not found"
-            />
-            <p>iPhone 13</p>
-          </div>
-        </div>
-        <div className={style.containerForIphone}>
-          <p className={style.new}>NEW</p>
-          <div className={style.imageWithText}>
-            <img
-              src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/13858/ip13_mini.png@webp"
-              alt="not found"
-            />
-            <p>iPhone 13 mini</p>
-          </div>
-        </div>
-        <div className={style.containerForIphoneWithoutTopic}>
-          <div className={style.imageWithText}>
-            <img
-              src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/12707/iphone_12_black_246x200.png@webp"
-              alt="not found"
-            />
-            <p>iPhone 12</p>
-          </div>
-        </div>
-        <div className={style.containerForIphone}>
-          <div className={style.discount}>
-            <p>АКЦИЯ</p>
-          </div>
-          <div className={style.imageWithText}>
-            <img
-              src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/12709/iphone_12_pro_graphite_246x200.png@webp"
-              alt="not found"
-            />
-            <p>iPhone 12 Pro</p>
-          </div>
-        </div>
-        <div className={style.containerForIphone}>
-          <div className={style.discount}>
-            <p>АКЦИЯ</p>
-          </div>
-          <div className={style.imageWithText}>
-            <img
-              src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/12710/iphone_12_pro_max_gold_246x200.png@webp"
-              alt="not found"
-            />
-            <p>iPhone 12 Pro</p>
-          </div>
-        </div>
-        <div className={style.containerForIphoneWithoutTopic}>
-          <div className={style.imageWithText}>
-            <img
-              src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/12708/iphone_12_mini_white_246x200.png@webp"
-              alt="not found"
-            />
-            <p>iPhone 12 mini</p>
-          </div>
-        </div>
-        <div className={style.containerForIphoneWithoutTopic}>
-          <div className={style.imageWithText}>
-            <img
-              src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/18/ipse_246x200.png@webp"
-              alt="not found"
-            />
-            <p>iPhone SE 2</p>
-          </div>
-        </div>
-        <div className={style.containerForIphoneWithoutTopic}>
-          <div className={style.imageWithText}>
-            <img
-              src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/10119/iphone_11.png@webp"
-              alt="not found"
-            />
-            <p>iPhone 11</p>
-          </div>
-        </div>
-        <div className={style.containerForIphoneWithoutTopic}>
-          <div className={style.imageWithText}>
-            <img
-              src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/4554/xr_ic.png@webp"
-              alt="not found"
-            />
-            <p>iPhone XR</p>
-          </div>
-        </div>
-        <div className={style.containerForIphoneWithoutTopic}>
-          <div className={style.imageWithText}>
-            <img
-              src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/12265/3-light-2.png@webp"
-              alt="not found"
-            />
-            <p> Услуги для iPhone</p>
-          </div>
-        </div>
-      </div>
+      <div className={style.listOfIphone}>{ourIphone(IphoneConfig)}</div>
       <footer>
         <img
           src="https://www.logolynx.com/images/logolynx/76/7655ad50e4860a1b3503a3756d919f6c.jpeg"
