56a8db7ab5749961ec01a276b819312c1bc9da8d
Vlad Ostapuk <Sp0ngbik@gmail.com>
2021-12-07 01:27:43 +0300

    map added

--- src/pages/mac/Mac.js
+++ src/pages/mac/Mac.js
@@ -5,12 +5,14 @@ const Mac = () => {
   const macMap = (arr) => {
     return arr.map((el, index) => {
       return (
-        <img
-          key={index}
-          src={el.picture}
-          className={el.className}
-          alt={el.altes}
-        ></img>
+        <a href="$">
+          <div className={el.atrClassName}>
+            <h1>{el.atrNew}</h1>
+            <h2 className={el.discClassName}>{el.atrDisc}</h2>
+          </div>
+          <img key={index} src={el.picture} alt={el.altes}></img>
+          <h3>{el.bottomText}</h3>
+        </a>
       );
     });
   };
@@ -26,64 +28,9 @@ const Mac = () => {
           <h2>Готовы к любым задачам</h2>
         </div>
       </div>
-      <div className={style.navBLock}>
-        <a href="$">{macMap(macPage)}</a>
-      </div>
+      <div className={style.navBLock}>{macMap(macPage)}</div>
     </>
   );
 };
 
 export default Mac;
-{
-  /* <div className={style.navBLock}>
-<a href="$">
-  <div className={style.attributes}>
-	<h1>NEW</h1>
-	<h2> АКЦИЯ</h2>
-  </div>
-  <img
-	src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/3/mcbookpro13.png@jpeg"
-	alt="not found"
-  ></img>
-  <h3>MacBookPro</h3>
-</a>
-<a href="$">
-  <img
-	src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/4/macbook_air_gold_with_m1_chip_2-up_print__usen.png@jpeg"
-	alt="not found"
-  ></img>
-  <h3>MacBookAir</h3>
-</a>
-<a href="$">
-  <div className={style.attributes}>
-	<h1>NEW</h1>
-  </div>
-  <img
-	src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/5/1.png@jpeg"
-	alt="not found"
-  ></img>
-  <h3>IMac</h3>
-</a>
-<a href="$">
-  <img
-	src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/7/amm_new_1.png@jpeg"
-	alt="not found"
-  ></img>
-  <h3>Mac Mini</h3>
-</a>
-<a href="$">
-  <img
-	src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/6/macpr.png@jpeg"
-	alt="not found"
-  ></img>
-  <h3>Mac Pro</h3>
-</a>
-<a href="$">
-  <img
-	src="https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/12263/1-light.png@jpeg"
-	alt="not found"
-  ></img>
-  <h3>Услуги для Mac</h3>
-</a>
-</div> */
-}
--- src/pages/mac/macConfig.js
+++ src/pages/mac/macConfig.js
@@ -1,40 +1,47 @@
 import style from "./style.module.scss";
 const macPage = [
   {
+    atrClassName: style.attributes,
+    atrNew: "NEW",
+    atrDisc: "АКЦИЯ",
+    discClassName: style.disc,
     picture:
       "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/3/mcbookpro13.png@jpeg",
-    className: style.pic,
     altes: "not found",
+    bottomText: "MacBookPro",
   },
   {
     picture:
       "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/4/macbook_air_gold_with_m1_chip_2-up_print__usen.png@jpeg",
-    className: style.pic,
+
     altes: "not found",
+    bottomText: "MacBookAir",
   },
   {
+    atrNew: "NEW",
+    atrClassName: style.attributes,
     picture:
       "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/5/1.png@jpeg",
-    className: style.pic,
     altes: "not found",
+    bottomText: "IMac",
   },
   {
     picture:
       "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/7/amm_new_1.png@jpeg",
-    className: style.pic,
     altes: "not found",
+    bottomText: "Mac Mini",
   },
   {
     picture:
       "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/6/macpr.png@jpeg",
-    className: style.pic,
     altes: "not found",
+    bottomText: "Mac Pro",
   },
   {
     picture:
       "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/12263/1-light.png@jpeg",
-    className: style.pic,
     altes: "not found",
+    bottomText: "Услуги для Mac",
   },
 ];
 
