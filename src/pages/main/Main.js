ec4eae2e4c8fe50d875befe9742de6668d9d6c46
Vitaly Tochenyy <Antisvinn@yadnex.by>
2021-12-07 20:26:32 +0300

    antd added

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,16 +1,16 @@
-import style from "./style.module.scss";
-import IStoreInfo from "../../components/IStoreBlockInfo/IStoreInfo";
-import Devices from "../../components/devices/Devices";
-import DeliveryWays from "../../components/iStoreWaysDelivery/DeliveryWays";
+import style from './style.module.scss';
+import IStoreInfo from '../../components/IStoreBlockInfo/IStoreInfo';
+import Devices from '../../components/devices/Devices';
+import DeliveryWays from '../../components/iStoreWaysDelivery/DeliveryWays';
 
-const Main = () => {
-  return (
-    <div className={style.container}>
-      <Devices />
-      <IStoreInfo />
-      <DeliveryWays />
-    </div>
-  );
+const Main = (props) => {
+	return (
+		<div className={style.container}>
+			<Devices />
+			<IStoreInfo />
+			<DeliveryWays />
+		</div>
+	);
 };
 
 export default Main;
