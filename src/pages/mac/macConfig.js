a0bd1276a1ec0d8364446d0d51a776cda5f2a368
Vitaly Tochenyy <Antisvinn@yadnex.by>
2021-12-07 19:35:09 +0300

    label fixes

--- src/pages/mac/macConfig.js
+++ src/pages/mac/macConfig.js
@@ -1,48 +1,48 @@
-import style from "./style.module.scss";
+import style from './style.module.scss';
 const macPage = [
-  {
-    atrClassName: style.attributes,
-    atrNew: "NEW",
-    atrDisc: "АКЦИЯ",
-    discClassName: style.disc,
-    picture:
-      "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/3/mcbookpro13.png@jpeg",
-    altes: "not found",
-    bottomText: "MacBookPro",
-  },
-  {
-    picture:
-      "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/4/macbook_air_gold_with_m1_chip_2-up_print__usen.png@jpeg",
+	{
+		atrClassName: style.attributes,
+		atrNew: 'NEW',
+		atrDisc: 'АКЦИЯ',
+		discClassName: style.disc,
+		picture:
+			'https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/3/mcbookpro13.png@jpeg',
+		altes: 'not found',
+		bottomText: 'MacBookPro',
+	},
+	{
+		picture:
+			'https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/4/macbook_air_gold_with_m1_chip_2-up_print__usen.png@jpeg',
 
-    altes: "not found",
-    bottomText: "MacBookAir",
-  },
-  {
-    atrNew: "NEW",
-    atrClassName: style.attributes,
-    picture:
-      "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/5/1.png@jpeg",
-    altes: "not found",
-    bottomText: "IMac",
-  },
-  {
-    picture:
-      "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/7/amm_new_1.png@jpeg",
-    altes: "not found",
-    bottomText: "Mac Mini",
-  },
-  {
-    picture:
-      "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/6/macpr.png@jpeg",
-    altes: "not found",
-    bottomText: "Mac Pro",
-  },
-  {
-    picture:
-      "https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/12263/1-light.png@jpeg",
-    altes: "not found",
-    bottomText: "Услуги для Mac",
-  },
+		altes: 'not found',
+		bottomText: 'MacBookAir',
+	},
+	{
+		atrNew: 'NEW',
+		atrClassName: style.attributes,
+		picture:
+			'https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/5/1.png@jpeg',
+		altes: 'not found',
+		bottomText: 'IMac',
+	},
+	{
+		picture:
+			'https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/7/amm_new_1.png@jpeg',
+		altes: 'not found',
+		bottomText: 'Mac Mini',
+	},
+	{
+		picture:
+			'https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/6/macpr.png@jpeg',
+		altes: 'not found',
+		bottomText: 'Mac Pro',
+	},
+	{
+		picture:
+			'https://cdn0.i-store.by/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:0:100:0/plain/s3://catalog-categories/7/12263/1-light.png@jpeg',
+		altes: 'not found',
+		bottomText: 'Услуги для Mac',
+	},
 ];
 
 export default macPage;
