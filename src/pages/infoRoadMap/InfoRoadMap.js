e54a2558ec0b473119005e12978936078a21e461
Vitaly Tochenyy <Antisvinn@yadnex.by>
2021-12-17 20:03:42 +0300

    axios added

--- src/pages/infoRoadMap/InfoRoadMap.js
+++ src/pages/infoRoadMap/InfoRoadMap.js
@@ -1,24 +1,24 @@
-import { Route, Switch } from "react-router";
-import Credits from "../credits/Credits";
-import Information from "../information/Information";
-import About from "../about/About";
-import TermsOfSale from "../terms-of-sale/TermsOfSale";
-import LegalEnt from "../legalEntities/LegalEnt";
-import Job from "../job/Job";
+import { Route, Switch } from 'react-router';
+import Credits from '../credits/Credits';
+import Information from '../information/Information';
+import About from '../about/About';
+import TermsOfSale from '../terms-of-sale/TermsOfSale';
+import LegalEnt from '../legalEntities/LegalEnt';
+import Job from '../job/Job';
 
 const InfoRoadMap = () => {
-  return (
-    <>
-      <Switch>
-        <Route path="/information/terms-of-sale" component={TermsOfSale} />
-        <Route path="/information/about" component={About} />
-        <Route path="/information/legEntities" component={LegalEnt} />
-        <Route path="/information/installment-and-credit" component={Credits} />
-        <Route path="/information/job" component={Job} />
-        <Route path="/information" component={Information} />
-      </Switch>
-    </>
-  );
+	return (
+		<>
+			<Switch>
+				<Route path='/information/terms-of-sale' component={TermsOfSale} />
+				<Route path='/information/about' component={About} />
+				<Route path='/information/legEntities' component={LegalEnt} />
+				<Route path='/information/installment-and-credit' component={Credits} />
+				<Route path='/information/job' component={Job} />
+				<Route path='/information' component={Information} />
+			</Switch>
+		</>
+	);
 };
 
 export default InfoRoadMap;
